c0473d569aa8fa7a2fc828399975d076aca22962
Alex Jacobsen <alex@siffin.co.nz>
2019-05-07 17:35:08 +1200

    Time: Added current time to overlay.

--- javascript.js
+++ javascript.js
@@ -4,8 +4,8 @@
     (function getTemperatures() {
 
         function populateWeatherPanel(temp_max, temp_min) {
-            $('#temp_max').text("H " + temp_max + "°");
-            $('#temp_min').text("L " + temp_min + "°");
+            $('#temp_max').text("H" + temp_max + "°");
+            $('#temp_min').text("L" + temp_min + "°");
         }
 
         window.callBackLocalWeather = function(json) {
@@ -13,12 +13,36 @@
             var temp_min = json.days[0].min;
 
             populateWeatherPanel(temp_max, temp_min);
-          }
-          
-          $.ajax({
+        }
+
+        $.ajax({
             url: "https://whklive.co.nz/local_forecast_today.json",
             dataType: "jsonp"
-          });
+        });
+
+    })();
+    "use strict"; // Start of use strict
+
+    (function getTime() {
+
+        function startTime() {
+            var today = new Date();
+            var h = today.getHours();
+            var m = today.getMinutes();
+            var s = today.getSeconds();
+            // h = checkTime(h);
+            m = checkTime(m);
+            s = checkTime(s);
+            var time_now = h + ":" + m + ":" + s;
+            $('#time').text(time_now);
+            var t = setTimeout(startTime, 1000);
+        }
+        function checkTime(i) {
+            if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+            return i;
+        }
+
+        startTime();
 
     })();
 
