dc2ee400c77515f5129b32594878e72278d761d2
Alex Jacobsen <alex@siffin.co.nz>
2019-05-18 20:33:32 +1200

    Temperature: Changed temperature to also update just after midnight and midday.
    Refactor: Changed code so that the timer is separated
    from the clock.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 (function ($) {
     "use strict"; // Start of use strict
 
-    function getTemperatures() {
+    function updateTemperatures() {
 
         function populateWeatherPanel(temp_max, temp_min) {
             $('#temp_max').text("H" + temp_max + "°");
@@ -19,17 +19,30 @@
             url: "https://whklive.co.nz/local_forecast_today.json",
             dataType: "jsonp"
         });
-
     }
 
-    getTemperatures();
+    function checkAndAddAnExtraZeroToTheBeginningOfAOneDigitNumber(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
+
+    function updateTime(time_now) {
+        $('#time').text(time_now);
+    }
+
+    function doThingsEverySecond(time_now) {
+        updateTime(time_now);
+
+        whenUpdateTemperatures(time_now);
+    }
+
+    function whenUpdateTemperatures(time_now) {
+            if (time_now == "00:15:00" || time_now == "12:15:00") {
+                updateTemperatures();
+            }
+    }
 
     function startTimeUpdateLoop() {
-
-        function checkAndAddAnExtraZeroToTheBeginningOfAOneDigitNumber(i) {
-            if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-            return i;
-        }
 
         function timeUpdateLoop() {
             var today = new Date();
@@ -38,20 +51,22 @@
             var s = today.getSeconds();
             // h = checkTime(h);
 
+            h = checkAndAddAnExtraZeroToTheBeginningOfAOneDigitNumber(h);
             m = checkAndAddAnExtraZeroToTheBeginningOfAOneDigitNumber(m);
             s = checkAndAddAnExtraZeroToTheBeginningOfAOneDigitNumber(s);
 
             var time_now = h + ":" + m + ":" + s;
 
-            $('#time').text(time_now);
+            doThingsEverySecond(time_now);
 
             setTimeout(timeUpdateLoop, 1000);
         }
 
         timeUpdateLoop();
-
     }
 
     startTimeUpdateLoop();
+
+    updateTemperatures();
 
 })(jQuery); // End of use strict
